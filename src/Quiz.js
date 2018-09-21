c1d1890a219e8d3f3c5b687be8694960d0e85ec5
David Vargas <davidvargash@gmail.com>
2018-09-21 10:46:43 -0400

    Quiz next question logic + Correct answer logic

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,9 +1,7 @@
 // Importing the React library into the Quiz Component
 import React, { Component } from "react";
-
 // Importing QuizQuestion into the Quiz file
 import QuizQuestion from "./QuizQuestion.js";
-
 // Importing QuizEnd into the Quiz file
 import QuizEnd from "./QuizEnd.js";
 
@@ -12,19 +10,26 @@ let quizData = require("./quiz_data.json");
 
 // Creating the QuizQuestion class
 class Quiz extends Component {
+
   constructor(props) {
     super(props);
     this.state = { quiz_position: 1 };
   }
 
+  showNextQuestion() {
+    this.setState((state) => {
+      return { quiz_position: state.quiz_position + 1};
+    });
+  }
+
   // Rendering the QuizQuestion component
   render() {
     const isQuizEnd = (this.state.quiz_position - 1 === quizData.quiz_questions.length);
-
     return (
-      (isQuizEnd) ? <QuizEnd /> :
       <div>
-        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+        {(isQuizEnd) ? <QuizEnd /> :
+          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+            showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
       </div>
     );
   }
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,16 +1,17 @@
 // Importing the React library into the QuizQuestionButton component
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 // Adding a render() method to the QuizQuestionButton class
-class QuizQuestionButton extends Component{
-    constructor(props){
-        super(props);
+class QuizQuestionButton extends Component {
+
+    handleClick() {
+        this.props.clickHandler(this.props.button_text);
     }
 
-    render(){
+    render() {
         return (
             <li>
-                <button>{this.props.button_text}</button>
+                <button onClick={this.handleClick.bind(this)}>{this.props.button_text}</button>
             </li>
         );
     }
